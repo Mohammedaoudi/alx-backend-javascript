6bd045f44281c235655c81b3042d3eaa98edfa38
Mohammedaoudi <meddaou2003@gmail.com>
2024-12-22 19:38:16 +0000

    daodo

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,13 @@
-console.log('Welcome to Holberton School, What is your name?');
-process.stdin.on('readable', () => {
-  const name = process.stdin.read();
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-  if (name) {
-    process.stdout.write(`Your name is: ${name}`);
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
